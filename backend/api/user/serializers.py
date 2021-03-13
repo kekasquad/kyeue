b317b4e9ff245c0c6cc7d06d3b1cee4d3b7584ab
Ivan Angelov <angelow.van@yandex.ru>
2021-03-13 19:54:06 +0300

    Added User API tests

--- backend/api/user/serializers.py
+++ backend/api/user/serializers.py
@@ -57,6 +57,14 @@ class UserUpdateSerializer(serializers.ModelSerializer):
         source='last_name'
     )
 
+    def update(self, instance, validated_data):
+        instance = super().update(instance, validated_data)
+        if 'password' in validated_data:
+            instance.set_password(validated_data['password'])
+            instance.save()
+
+        return instance
+
 
 class UserRetrieveSerializer(serializers.ModelSerializer):
     class Meta:
--- backend/api/user/tests.py
+++ backend/api/user/tests.py
@@ -1,8 +1,9 @@
 from django.test import TestCase
 from django.urls import reverse
+from factory import fuzzy
 from rest_framework import status
 
-from core.factories import UserFactory
+from core.models import User
 from ..tests import AuthMixin
 
 
@@ -16,10 +17,48 @@ class UserAPITestCases(AuthMixin, TestCase):
         )
 
     def test_delete_user(self):
-        user = UserFactory()
 
         response = self.client.delete(
-            self._get_url(user.id),
+            self._get_url(self.user.id),
             HTTP_AUTHORIZATION=self.access_header
         )
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(User.objects.count(), 0)
+
+    def test_retrieve_user(self):
+        response = self.client.get(
+            self._get_url(self.user.id),
+            HTTP_AUTHORIZATION=self.access_header
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        response = response.json()
+
+        self.assertEqual(response['id'], str(self.user.id))
+        self.assertEqual(response['username'], self.user.username)
+        self.assertEqual(response['firstName'], self.user.first_name)
+        self.assertEqual(response['lastName'], self.user.last_name)
+
+    def test_update_user_successful(self):
+        fuzzy_text = fuzzy.FuzzyText()
+        update_data = {
+            'username': fuzzy_text.fuzz(),
+            'password': fuzzy_text.fuzz(),
+            'firstName': fuzzy_text.fuzz(),
+            'lastName': fuzzy_text.fuzz(),
+        }
+        response = self.client.patch(
+            self._get_url(self.user.id),
+            data=update_data,
+            content_type='application/json',
+            HTTP_AUTHORIZATION=self.access_header
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        response = response.json()
+
+        self.assertEqual(response['firstName'], update_data['firstName'])
+        self.assertEqual(response['lastName'], update_data['lastName'])
+
+        self.user.refresh_from_db()
+        self.assertTrue(self.user.check_password(update_data['password']))
+        self.assertEqual(self.user.first_name, update_data['firstName'])
+        self.assertEqual(self.user.last_name, update_data['lastName'])
