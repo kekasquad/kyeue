dd1ba696d9db16f74c9b73c8e846a98ab745faeb
Vladislav <vlad@grokhotov.ru>
2021-03-14 04:53:45 +0300

    iOS init

---
+++ ios/kyeue/kyeue/ViewControllers/SignVC.swift
@@ -0,0 +1,19 @@
+//
+//  SignVC.swift
+//  kyeue
+//
+//  Created by Vladislav Grokhotov on 14.03.2021.
+//
+
+import UIKit
+
+class SignVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
