a752a88283361ebd1221730d9868aa2e0c4ee691
Vladislav <vlad@grokhotov.ru>
2021-04-06 23:12:24 +0300

    implement web sockets

--- ios/kyeue/kyeue/Models/WebSocketMessage.swift
+++ ios/kyeue/kyeue/Models/WebSocketMessage.swift
@@ -7,22 +7,81 @@
 
 import Foundation
 
-struct WebSocketMessage {
+struct WebSocketMessage: Codable {
     let type: MessageType
-    let message: String
+    let message: Message
 }
 
-enum MessageType: String {
+enum MessageType: String, Codable {
     case memberOperation = "member_operation" // в месадже будет { что-то: id } - userId
     case queueOperation = "queue_operation" // в месадже будет { create_queue: id } - qveveId
 }
 
-enum MemberOperation: String {
-    case pushMember = "push_member"
-    case popMember = "pop_member"
-    case moveMemberToTheEnd = "move_member_to_the_end"
+enum Message: Codable {
+    
+    case pushMember(String)
+    case popMember(String)
+    case moveMemberToTheEnd(String)
+    case skipTurn(String)
+    case createQueue(String)
+    case deleteQueue(String)
+    
+    enum CodingKeys: String, CodingKey {
+        case  pushMember = "push_member", popMember = "pop_member", moveMemberToTheEnd = "move_member_to_the_end", createQueue = "create_queue", deleteQueue="delete_queue", skipTurn="skip_turn"
+    }
 }
 
-enum QueueOperation: String {
-    case createQueue = "create_queue"
+extension Message {
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        switch self {
+        case let .pushMember(value):
+            try container.encode(value, forKey: CodingKeys.pushMember)
+        case let .popMember(value):
+            try container.encode(value, forKey: CodingKeys.popMember)
+        case let .moveMemberToTheEnd(value):
+            try container.encode(value, forKey: CodingKeys.moveMemberToTheEnd)
+        case let .skipTurn(value):
+            try container.encode(value, forKey: CodingKeys.skipTurn)
+        case let .createQueue(value):
+            try container.encode(value, forKey: CodingKeys.createQueue)
+        case let .deleteQueue(value):
+            try container.encode(value, forKey: CodingKeys.deleteQueue)
+        }
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let containerKeys = Set(container.allKeys)
+        
+        let push = Set<CodingKeys>([.pushMember])
+        let pop = Set<CodingKeys>([.popMember])
+        let move = Set<CodingKeys>([.moveMemberToTheEnd])
+        let skip = Set<CodingKeys>([.skipTurn])
+        let create = Set<CodingKeys>([.createQueue])
+        let delete = Set<CodingKeys>([.deleteQueue])
+        
+        switch containerKeys {
+        case push:
+            let id = try container.decode(String.self, forKey: .pushMember)
+            self = .pushMember(id)
+        case pop:
+            let id = try container.decode(String.self, forKey: .popMember)
+            self = .popMember(id)
+        case move:
+            let id = try container.decode(String.self, forKey: .moveMemberToTheEnd)
+            self = .moveMemberToTheEnd(id)
+        case skip:
+            let id = try container.decode(String.self, forKey: .skipTurn)
+            self = .skipTurn(id)
+        case create:
+            let id = try container.decode(String.self, forKey: .createQueue)
+            self = .createQueue(id)
+        case delete:
+            let id = try container.decode(String.self, forKey: .deleteQueue)
+            self = .deleteQueue(id)
+        default:
+            self = .createQueue("i")
+        }
+    }
 }
---
+++ ios/kyeue/kyeue/Network/WebSocketsService.swift
@@ -0,0 +1,217 @@
+//
+//  WebSocketsService.swift
+//  kyeue
+//
+//  Created by Vladislav Grokhotov on 06.04.2021.
+//
+
+import Foundation
+
+class MembersWebSocketsService {
+    
+    private var push: ((String) -> Void)?
+    private var pop: ((String) -> Void)?
+    private var move: ((String) -> Void)?
+    private var skip: ((String) -> Void)?
+    private let queueId: String
+
+    init(queueId: String){
+        self.queueId = queueId
+    }
+    
+    func set(
+        push: @escaping (String) -> Void,
+        pop: @escaping (String) -> Void,
+        move: @escaping (String) -> Void,
+        skip: @escaping (String) -> Void)
+    {
+        self.push = push
+        self.pop = pop
+        self.move = move
+        self.skip = skip
+    }
+    
+    private lazy var webSocketTask =
+        URLSession(configuration: .default)
+        .webSocketTask(with: URL(string: "ws://127.0.0.1:8080/ws/queue/\(self.queueId)/")!)
+    
+    public func connect() {
+        webSocketTask.resume()
+    }
+    
+    public func disconnect(){
+        webSocketTask.cancel(with: .goingAway, reason: nil)
+    }
+    
+    func ping() {
+        webSocketTask.sendPing { (error) in
+            if let error = error {
+                print("Ping failed: \(error)")
+            } else {
+                self.scheduleNextPing()
+            }
+        }
+    }
+
+    private func scheduleNextPing() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 10) { [weak self] in
+            self?.ping()
+        }
+    }
+    
+    
+    
+    func receiveData() {
+        
+        webSocketTask.receive { result in
+            switch result {
+            case .failure(let error):
+                print("Error in receiving message: \(error)")
+            case .success(let message):
+                print("success receive")
+                switch message {
+                case .string(let text):
+                    print("Received text: \(text)")
+                case .data(let data):
+                    print("Received data: \(data)")
+                    
+                    if let message = try? JSONDecoder().decode(WebSocketMessage.self, from: data) {
+                        switch message.message {
+                        case let .popMember(value):
+                            DispatchQueue.main.async { [weak self] in
+                                self?.pop?(value)
+                            }
+                        case let .pushMember(value):
+                            DispatchQueue.main.async { [weak self] in
+                                self?.push?(value)
+                            }
+                        case let .moveMemberToTheEnd(value):
+                            DispatchQueue.main.async { [weak self] in
+                                self?.move?(value)
+                            }
+                        case let .skipTurn(value):
+                            DispatchQueue.main.async { [weak self] in
+                                self?.skip?(value)
+                            }
+                        default:
+                            break
+                        }
+                    }
+                    
+                @unknown default:
+                    debugPrint("Unknown message")
+                }
+                
+                self.receiveData() // рекурсия
+            }
+        }
+    }
+}
+
+class QueuesWebSocketsService {
+    
+    public static let shared = QueuesWebSocketsService() // создаем Синглтон
+    private init(){}
+    
+    private var create: ((String) -> Void)?
+    private var delete: ((String) -> Void)?
+    
+    func set(create: @escaping (String) -> Void, delete: @escaping (String) -> Void) {
+        self.create = create
+        self.delete = delete
+    }
+    
+    private let webSocketTask =
+        URLSession(configuration: .default)
+        .webSocketTask(with: URL(string: "ws://127.0.0.1:8080/ws/notifications/")!)
+    
+    public func connect() {
+        webSocketTask.resume()
+    }
+    
+    public func disconnect(){
+        webSocketTask.cancel(with: .goingAway, reason: nil)
+    }
+    
+    public func ping() {
+        webSocketTask.sendPing { (error) in
+            if let error = error {
+                print("Ping failed: \(error)")
+            } else {
+                self.scheduleNextPing()
+            }
+        }
+    }
+
+    private func scheduleNextPing() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5) { [weak self] in
+            self?.ping()
+        }
+    }
+    
+    public func create(queueID: String) {
+        webSocketTask.resume()
+        
+        let message = WebSocketMessage(type: .queueOperation, message: .createQueue(queueID))
+        if let messageData = try? JSONEncoder().encode(message) {
+            webSocketTask.send(URLSessionWebSocketTask.Message.data(messageData)) { (error) in
+                if let error = error {
+                    print("cannot send message because of \(error.localizedDescription)")
+                }
+            }
+        }
+    }
+    
+    public func delete(queueID: String) {
+        webSocketTask.resume()
+        
+        let message = WebSocketMessage(type: .queueOperation, message: .deleteQueue(queueID))
+        if let messageData = try? JSONEncoder().encode(message) {
+            webSocketTask.send(URLSessionWebSocketTask.Message.data(messageData)) { (error) in
+                if let error = error {
+                    print("cannot send message because of \(error.localizedDescription)")
+                }
+            }
+        }
+    }
+    
+    func receiveData() {
+        
+        webSocketTask.receive { result in
+            switch result {
+            case .failure(let error):
+                print("Error in receiving message: \(error)")
+            case .success(let message):
+                print("success receive")
+                switch message {
+                case .string(let text):
+                    print("Received text: \(text)")
+                case .data(let data):
+                    print("Received data: \(data)")
+                    
+                    if let message = try? JSONDecoder().decode(WebSocketMessage.self, from: data) {
+                        switch message.message {
+                        case let .createQueue(value):
+                            DispatchQueue.main.async { [weak self] in
+                                self?.create?(value)
+                            }
+                        case let .deleteQueue(value):
+                            DispatchQueue.main.async { [weak self] in
+                                self?.delete?(value)
+                            }
+                        default:
+                            break
+                        }
+                    }
+                    
+                @unknown default:
+                    debugPrint("Unknown message")
+                }
+                
+                self.receiveData() // рекурсия
+            }
+        }
+    }
+    
+    
+}
